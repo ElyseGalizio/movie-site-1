6946b95d82f6b239f418bef2cff350a8c4ab76c4
Elyse Galizio <elyse.galizio@gmail.com>
2023-03-27 11:36:13 +0200

    added pseudo code about project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
+//Popular movies rendered on main page load
+//Scroll through pages of movies
+//Clicking on a movie redirects to details page via react router
+//Create Contact or About page
+//Add React Router function in Navbar
+//Update Footer
 function App() {
   return (
     <div className="App">
