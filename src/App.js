b2af8f8acb6d9d58902eca0b4130b3c8b7e606d5
Elyse Galizio <elyse.galizio@gmail.com>
2023-03-27 11:49:58 +0200

    created basic movie card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './reset.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import MovieCard from './components/MovieCard/MovieCard';
 
 //Popular movies rendered on main page load
 //Scroll through pages of movies
@@ -13,6 +14,10 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <MovieCard
+        image='https://image.tmdb.org/t/p/w500/gOnmaxHo0412UVr1QM5Nekv1xPi.jpg'
+        name='Cocaine Bear'
+      />
       <Footer />
     </div>
   );
