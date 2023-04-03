a8ab97b6bff754118de55cf12d8de6eb7a4036bc
Elyse Galizio <elyse.galizio@gmail.com>
2023-04-03 19:39:13 +0200

    fixed image on about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,13 +4,16 @@ function About() {
     return (
         <div className="App">
             <h1 className='app-head-container'>About Page</h1>
-            <p className="about-page">
-                EFG Films is a movie app project by Elyse Galizio.
-                The company is named after her children.
-                All images and movie information are courtesy of The Movie Database
-            </p>
-            <img src='./components/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg'
-                alt='The Movie Database logo' />
+            <div className="about-page">
+                <p>EFG Films is a movie app project by Elyse Galizio.
+                    The company is named after her children.
+                    <br />
+                    All images and movie information are courtesy of The Movie Database.</p>
+                <img src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg'
+                    alt='The Movie Database logo'
+                    className='about-image'
+                />
+            </div>
         </div>
     )
 }
