373104402911cee1b9780491c6b2e2632521369a
Elyse Galizio <elyse.galizio@gmail.com>
2023-04-03 14:28:58 +0200

    code added for search feature but is commented out for now

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import './About.css';
 function About() {
     return (
         <div className="App">
-            <h1>About Page</h1>
+            <h1 className='app-head-container'>About Page</h1>
             <p className="about-page">
                 EFG Films is a movie app project by Elyse Galizio.
                 The company is named after her children.
